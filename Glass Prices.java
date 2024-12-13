e76961fbbd85a0eab746b8ec551db8d19313cf30
Ramya <ramyareddym023@gmail.com>
2024-12-13 21:59:41 +0530

    Glass Prices.java

---
+++ Glass Prices.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int x=sc.nextInt();
+		int y=sc.nextInt();
+		if(y<=(x+x))
+		System.out.println("METAL");
+		else
+        System.out.println("PLASTIC");
+	}
+}
