024a202fc84d682308724d66ee1d148195607ea6
Ramya <ramyareddym023@gmail.com>
2024-12-15 23:37:21 +0530

    Create Healthy Sleep.java

---
+++ Healthy Sleep.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+    Scanner sc=new Scanner(System.in);
+    int a=sc.nextInt();
+    if(a<8)
+    System.out.println("LESS");
+    else if(a==8)
+    System.out.println("PERFECT");
+    else
+    System.out.println("MORE");
+	}
+}
