1a8ea4b6f4c429b7abea168d22de2083f3506ff9
Ramya <ramyareddym023@gmail.com>
2024-12-15 23:03:03 +0530

    Create Advitiya.java

---
+++ Advitiya.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	Scanner sc=new Scanner(System.in);
+	int a=sc.nextInt();
+	if(a==16||a==17||a==18)
+	System.out.println("ADVITIYA");
+	else
+	System.out.println("WAITING FOR ADVITIYA");
+
+	}
+}
