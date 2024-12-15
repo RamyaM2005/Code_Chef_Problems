f44d21548fb853d31e3bb65a3cc6962dfb668be7
Ramya <ramyareddym023@gmail.com>
2024-12-15 23:44:59 +0530

    Create Water Consumption.java

---
+++ Water Consumption.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+
+class Codechef
+{
+	public static void main (String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		while(t-->0)
+		{
+    		int s=sc.nextInt();
+    		if(s>=2000)
+    		System.out.println("YES");
+    		else
+    		System.out.println("NO");
+		}
+		
+	}
+}
